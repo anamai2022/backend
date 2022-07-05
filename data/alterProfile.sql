29caa0a4174975bdbdb517d14918dcf336d5e499
Mr. Unknown <teenactindicators@gmail.com>
2022-07-05 15:19:36 +0700

    commit update

--- data/alterProfile.sql
+++ data/alterProfile.sql
@@ -15,3 +15,7 @@ ALTER TABLE `yfhsApp`.`tbl_Contact`
 ADD COLUMN `f_organization` VARCHAR(100) NULL AFTER `f_line`,
 ADD COLUMN `f_institution` VARCHAR(100) NULL AFTER `f_organization`,
 ADD COLUMN `f_position` VARCHAR(100) NULL AFTER `f_institution`;
+
+ALTER TABLE `yfhsApp`.`tbl_Contact` 
+CHANGE COLUMN `f_createBy` `f_createBy` VARCHAR(100) NULL DEFAULT NULL ,
+CHANGE COLUMN `f_updateBy` `f_updateBy` VARCHAR(100) NULL DEFAULT NULL ;
