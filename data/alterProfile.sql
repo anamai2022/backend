2cfa634dea3cea5e486408ef5cc1235a3028bac2
Mr. Unknown <teenactindicators@gmail.com>
2022-07-06 21:00:53 +0700

    commit update

--- data/alterProfile.sql
+++ data/alterProfile.sql
@@ -25,3 +25,11 @@ ALTER TABLE `yfhsApp`.`tbl_Profile`
 CHANGE COLUMN `f_director` `f_director` VARCHAR(100) NULL DEFAULT NULL ,
 CHANGE COLUMN `f_physician` `f_physician` VARCHAR(100) NULL DEFAULT NULL ,
 CHANGE COLUMN `f_responsiblePerson` `f_responsiblePerson` VARCHAR(100) NULL DEFAULT NULL ;
+
+
+ALTER TABLE `yfhsApp`.`tbl_Questionnaire` 
+ADD COLUMN `f_questionWtMain` INT NULL DEFAULT 0 AFTER `f_description`,
+ADD COLUMN `f_questionWtSub` INT NULL DEFAULT 0 AFTER `f_questionWtMain`;
+
+ALTER TABLE `yfhsApp`.`tbl_Questionnaire` 
+CHANGE COLUMN `f_questionWtSub` `f_questionWtSub` FLOAT NULL DEFAULT '0' ;
--- schema/questionnaire/index.js
+++ schema/questionnaire/index.js
@@ -1,115 +1,122 @@
 module.exports = (sequelize, Sequelize) => {
-    const Questionnaire = sequelize.define(
-      "tbl_Questionnaire",
-      {
-        f_code: {
-          type: Sequelize.INTEGER,
-          field: "f_code",
-          primaryKey: true,
-        },
-        f_section: {
-          type: Sequelize.STRING,
-          field: "f_section",
-        },
-        f_question: {
-          type: Sequelize.STRING,
-          field: "f_question",
-        },
-        f_question_group: {
-          type: Sequelize.STRING,
-          field: "f_question_group",
-        },
-        f_sequence: {
-            type: Sequelize.STRING,
-            field: "f_sequence",
-          },
-        f_hadertitle: {
-          type: Sequelize.STRING,
-          field: "f_hadertitle",
-        },
-        f_title: {
-            type: Sequelize.STRING,
-            field: "f_title",
-        },    
-        
-        f_detail: {
-            type: Sequelize.STRING,
-            field: "f_detail",
-        },                  
-        f_request: {
-            type: Sequelize.STRING,
-            field: "f_request",
-        }, 
-        f_description: {
-            type: Sequelize.STRING,
-            field: "f_description",
-        }, 
-        f_upload_file: {
-            type: Sequelize.STRING,
-            field: "f_upload_file",
-        }, 
-        f_upload_image: {
-            type: Sequelize.STRING,
-            field: "f_upload_image",
-        }, 
-        f_address_url: {
-            type: Sequelize.STRING,
-            field: "f_address_url",
-        },                 
-        f_additional_message: {
-            type: Sequelize.STRING,
-            field: "f_additional_message",
-        },   
-        f_form: {
-            type: Sequelize.STRING,
-            field: "f_form",
-        },   
-        f_total: {
-            type: Sequelize.STRING,
-            field: "f_total",
-        },   
-        f_fromextra: {
-            type: Sequelize.STRING,
-            field: "f_fromextra",
-        },   
-        f_fromname: {
-            type: Sequelize.STRING,
-            field: "f_fromname",
-        },   
-        f_yeartotal: {
-            type: Sequelize.STRING,
-            field: "f_yeartotal",
-        },     
-        f_addyeartotal: {
-            type: Sequelize.STRING,
-            field: "f_addyeartotal",
-        }, 
-        f_createDate: {
-          type: Sequelize.STRING,
-          field: "f_createDate",
-        },
-        f_createBy: {
-          type: Sequelize.STRING,
-          field: "f_createBy",
-        },
-        f_updateDate: {
-            type: Sequelize.STRING,
-            field: "f_updateDate",
-          },
-        f_updateBy: {
-            type: Sequelize.STRING,
-            field: "f_updateBy",
-          },
-        f_status: {
-            type: Sequelize.STRING,
-            field: "f_status",
-          },
+  const Questionnaire = sequelize.define(
+    "tbl_Questionnaire",
+    {
+      f_code: {
+        type: Sequelize.INTEGER,
+        field: "f_code",
+        primaryKey: true,
       },
-      {
-        timestamps: false,
-        freezeTableName: true,
-      }
-    );
-    return Questionnaire;
-  };
-  
+      f_section: {
+        type: Sequelize.STRING,
+        field: "f_section",
+      },
+      f_question: {
+        type: Sequelize.STRING,
+        field: "f_question",
+      },
+      f_question_group: {
+        type: Sequelize.STRING,
+        field: "f_question_group",
+      },
+      f_sequence: {
+        type: Sequelize.STRING,
+        field: "f_sequence",
+      },
+      f_questionWtMain: {
+        type: Sequelize.STRING,
+        field: "f_questionWtMain",
+      },      
+      f_questionWtSub: {
+        type: Sequelize.STRING,
+        field: "f_questionWtSub",
+      },
+      f_hadertitle: {
+        type: Sequelize.STRING,
+        field: "f_hadertitle",
+      },
+      f_title: {
+        type: Sequelize.STRING,
+        field: "f_title",
+      },
+
+      f_detail: {
+        type: Sequelize.STRING,
+        field: "f_detail",
+      },
+      f_request: {
+        type: Sequelize.STRING,
+        field: "f_request",
+      },
+      f_description: {
+        type: Sequelize.STRING,
+        field: "f_description",
+      },
+      f_upload_file: {
+        type: Sequelize.STRING,
+        field: "f_upload_file",
+      },
+      f_upload_image: {
+        type: Sequelize.STRING,
+        field: "f_upload_image",
+      },
+      f_address_url: {
+        type: Sequelize.STRING,
+        field: "f_address_url",
+      },
+      f_additional_message: {
+        type: Sequelize.STRING,
+        field: "f_additional_message",
+      },
+      f_form: {
+        type: Sequelize.STRING,
+        field: "f_form",
+      },
+      f_total: {
+        type: Sequelize.STRING,
+        field: "f_total",
+      },
+      f_fromextra: {
+        type: Sequelize.STRING,
+        field: "f_fromextra",
+      },
+      f_fromname: {
+        type: Sequelize.STRING,
+        field: "f_fromname",
+      },
+      f_yeartotal: {
+        type: Sequelize.STRING,
+        field: "f_yeartotal",
+      },
+      f_addyeartotal: {
+        type: Sequelize.STRING,
+        field: "f_addyeartotal",
+      },
+      f_createDate: {
+        type: Sequelize.STRING,
+        field: "f_createDate",
+      },
+      f_createBy: {
+        type: Sequelize.STRING,
+        field: "f_createBy",
+      },
+      f_updateDate: {
+        type: Sequelize.STRING,
+        field: "f_updateDate",
+      },
+      f_updateBy: {
+        type: Sequelize.STRING,
+        field: "f_updateBy",
+      },
+      f_status: {
+        type: Sequelize.STRING,
+        field: "f_status",
+      },
+    },
+    {
+      timestamps: false,
+      freezeTableName: true,
+    }
+  );
+  return Questionnaire;
+};
