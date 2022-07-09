c9920eb1893cd3cfd9aaecf6204418f47ca55101
Mr. Unknown <teenactindicators@gmail.com>
2022-07-09 18:41:20 +0700

    commit update

--- routers/profile.js
+++ routers/profile.js
@@ -3,24 +3,6 @@ const app = express();
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const { authenticateJWT } = require('../middleware/authenticateJWT')
-// const authenticateJWT = (req, res, next) => {
-// 	const authHeader = req.headers.authorization;
-    
-// 	if (authHeader) {
-// 	    const token = authHeader.split(' ')[1];
-    
-// 	    jwt.verify(token, process.env.secret, (err, user) => {
-// 		if (err) {
-// 		    return res.sendStatus(403);
-// 		}
-    
-// 		req.user = user;
-// 		next();
-// 	    });
-// 	} else {
-// 	    res.sendStatus(401);
-// 	}
-//     };
 const profileController = require('../controllers/profileController');
 
 router.get("/:f_code",authenticateJWT,profileController.getProfile);
