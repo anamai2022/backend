b2350c3a8c85b02f663c0a23fcc67c4624c361ea
Mr. Unknown <teenactindicators@gmail.com>
2022-07-12 20:20:06 +0700

    commit update

--- data/alterProfile.sql
+++ data/alterProfile.sql
@@ -36,3 +36,31 @@ CHANGE COLUMN `f_questionWtSub` `f_questionWtSub` FLOAT NULL DEFAULT '0' ;
 
 ALTER TABLE `yfhsApp`.`tbl_Upload` 
 CHANGE COLUMN `f_surveyId` `f_docrunning` INT(11) NULL DEFAULT NULL ;
+
+
+ALTER TABLE `yfhsApp`.`tbl_Questionnaire` 
+DROP COLUMN `f_addyeartotal`,
+DROP COLUMN `f_total`,
+CHANGE COLUMN `f_yeartotal` `f_fromType` VARCHAR(150) NULL DEFAULT '0' ;
+
+CREATE TABLE `yfhsApp`.`tbl_formExtra` (
+  `f_code` INT NOT NULL AUTO_INCREMENT,
+  `f_fromType` VARCHAR(150) NULL,
+  `f_codeQuestion` INT NULL,
+  `f_section` VARCHAR(45) NULL,
+  `f_question` INT NULL,
+  `f_questionWtMain` INT NULL,
+  `f_questionWtSub` INT NULL,
+  `f_question_group` VARCHAR(45) NULL,
+  `f_sequence` INT NULL,
+  `f_hadertitle` INT NULL,
+  `f_title` VARCHAR(255) NULL,
+  `f_detail` VARCHAR(45) NULL,
+  `f_request` INT NULL,
+  `f_description` VARCHAR(280) NULL,
+  `f_createDate` DATETIME NULL,
+  `f_createBy` VARCHAR(150) NULL,
+  `f_updateDate` DATETIME NULL,
+  `f_updateBy` VARCHAR(150) NULL,
+  `f_status` TINYINT NULL,
+  PRIMARY KEY (`f_code`));
